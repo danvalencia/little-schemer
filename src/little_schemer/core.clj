ad54ab4921abafd09da2087b62cde2d0a91449c6
Daniel Valencia Backhoff <danvalencia@gmail.com>
2015-08-02 19:01:57 -0700

    More functions up to chapter 4

--- src/little_schemer/core.clj
+++ src/little_schemer/core.clj
@@ -1,36 +1,109 @@
 (ns little-schemer.core
   (:gen-class))
 
-(defn car
-  "Synonym for first"
-  [l]
-  (first l))
+(def car
+  (fn [l]
+    (first l)))
 
-(defn cdr
-  "Synonym for rest"
-  [l]
-  (rest l))
+(def cdr
+  (fn [l]
+    (rest l)))
 
-(defn atom?
-  "Returns true if argument is not a list, otherwise returns false"
-  [arg]
-  (not (list? arg)))
+(def atom?
+  (fn [a]
+    (not (seq? a))))
 
-(defn eq?
-  "Synonym for ="
-  [a b]
-  (= a b))
+(def eq?
+  (fn [a b]
+    (= a b)))
 
-(defn null?
-  "Synonym for empty?"
-  [l]
-  (empty? l))
+(def null?
+  (fn [l]
+   (or
+    (nil? l)
+    (= () l))))
 
-(defn lat?
-  "Returns true if neither element in the given array is a list"
-  [l]
-  (loop [l l]
+(def lat?
+  (fn [l]
     (cond
-      (empty? l) true
-      (atom? (car l)) (recur (cdr l))
+      (null? l) true
+      (atom? (car l)) (lat? (cdr l))
       :else false)))
+
+(def member?
+  (fn [a lat]
+    (cond
+      (null? lat) false
+      :else (or (eq? (car lat) a)
+                (member? a (cdr lat))))))
+
+(def rember
+  (fn [a lat]
+    (cond
+      (null? lat) '()
+      (eq? (car lat) a) (cdr lat)
+      :else (cons (car lat)
+                  (rember a (cdr lat))))))
+
+(def firsts
+  (fn [l]
+    (cond
+      (null? l) '()
+      :else (cons (car (car l))
+                   (firsts (cdr l))))))
+
+(def insertR
+  (fn [new old lat]
+    (cond
+      (null? lat) '()
+      (eq? (car lat) old) (cons old (cons new (cdr lat)))
+      :else (cons (car lat) (insertR new old (cdr lat))))))
+
+(def insertL
+  (fn [new old lat]
+    (cond
+      (null? lat) '()
+      (eq? (car lat) old) (cons new lat)
+      :else (cons (car lat) (insertL new old (cdr lat))))))
+
+(def subst
+  (fn [new old lat]
+    (cond
+      (null? lat) '()
+      (eq? (car lat) old) (cons new (cdr lat))
+      :else (cons (car lat) (subst new old (cdr lat))))))
+
+(def subst2
+  (fn [new o1 o2 lat]
+    (cond
+      (null? lat) '()
+      (or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat))
+      :else (cons (car lat) (subst2 new o1 o2 (cdr lat))))))
+
+(def multirember
+  (fn [a lat]
+    (cond
+      (null? lat) '()
+      (eq? a (car lat)) (multirember a (cdr lat))
+      :else (cons (car lat) (multirember a (cdr lat))))))
+
+(def multiinsertR
+  (fn [new old lat]
+    (cond
+      (null? lat) '()
+      (eq? old (car lat)) (cons old (cons new (multiinsertR new old (cdr lat))))
+      :else (cons (car lat) (multiinsertR new old (cdr lat))))))
+
+(def multiinsertL
+  (fn [new old lat]
+    (cond
+      (null? lat) '()
+      (eq? old (car lat)) (cons new (cons old (multiinsertL new old (cdr lat))))
+      :else (cons (car lat) (multiinsertL new old (cdr lat))))))
+
+(def multisubst
+  (fn [new old lat]
+    (cond
+      (null? lat) '()
+      (eq? old (car lat)) (cons new (multisubst new old (cdr lat)))
+      :else (cons (car lat) (multisubst new old (cdr lat))))))
