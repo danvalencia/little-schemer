4d8bfd9957883d535cdeab3a4f27755c88ce23a6
Daniel Valencia Backhoff <danvalencia@gmail.com>
2015-08-15 20:38:20 -0700

    Exercises up to page 87

--- src/little_schemer/core.clj
+++ src/little_schemer/core.clj
@@ -107,3 +107,190 @@
       (null? lat) '()
       (eq? old (car lat)) (cons new (multisubst new old (cdr lat)))
       :else (cons (car lat) (multisubst new old (cdr lat))))))
+
+(def add1
+  (fn [a]
+    (inc a)))
+
+(def sub1
+  (fn [a]
+    (dec a)))
+
+(def sum
+  (fn [a b]
+    (cond
+      (zero? b) a
+      :else (add1 (sum a (sub1 b))))))
+
+(def sub
+  (fn [a b]
+    (cond
+      (zero? b) a
+      :else (sub (sub1 a) (sub1 b)))))
+
+(def addtup
+  (fn [tup]
+    (cond
+      (null? tup) 0
+      :else (+ (car tup) (addtup (cdr tup))))))
+
+(def ×
+  (fn [n m]
+    (cond
+      (= 0 m) 0
+      :else (+ n (× n (sub1 m))))))
+
+(def tup+
+  (fn [tup1 tup2]
+    (cond
+      (null? tup1) tup2
+      (null? tup2) tup1
+      :else (cons (+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2))))))
+
+(def >
+  (fn [n m]
+    (cond
+      (zero? n) false
+      (zero? m) true
+      :else (> (sub1 n) (sub1 m)))))
+
+(def <
+  (fn [n m]
+    (cond
+      (zero? m) false
+      (zero? n) true
+      :else (< (sub1 n) (sub1 m)))))
+
+(def =
+  (fn [n m]
+    (cond
+      (> n m) false
+      (< n m) false
+      :else true)))
+
+(def ↑
+  (fn [n m]
+    (cond
+      (zero? m) 1
+      :else (× n (↑ n (sub1 m))))))
+
+(def ÷
+  (fn [n m]
+    (cond
+      (< n m) 0
+      :else (add1 (÷ (- n m) m)))))
+
+(def length
+  (fn [lat]
+    (cond
+      (null? lat) 0
+      :else (add1 (length (cdr lat))))))
+
+(def pick
+  (fn [n lat]
+    (cond
+      (zero? (sub1 n)) (car lat)
+      :else (pick (sub1 n) (cdr lat)))))
+
+(def rempick
+  (fn [n lat]
+    (cond
+      (zero? (sub1 n)) (cdr lat)
+      :else (cons (car lat) (rempick (sub1 n) (cdr lat))))))
+
+(def no-nums
+  (fn [lat]
+    (cond
+      (null? lat) '()
+      (number? (car lat)) (no-nums (cdr lat))
+      :else (cons (car lat) (no-nums (cdr lat))))))
+
+(def all-nums
+  (fn [lat]
+    (cond
+      (null? lat) '()
+      (number? (car lat)) (cons (car lat) (all-nums (cdr lat)))
+      :else (all-nums (cdr lat)))))
+
+(def eqan?
+  (fn [a1 a2]
+    (cond
+      (and (number? a1) (number? a2)) (= a1 a2)
+      (or (number? a1) (number? a2)) false
+      :else (eq? a1 a2))))
+
+(def occur
+  (fn [a lat]
+    (cond
+      (null? lat) 0
+      (eqan? a (car lat)) (add1 (occur a (cdr lat)))
+      :else (occur a (cdr lat)))))
+
+(def one?
+  (fn [n]
+    (= n 1)))
+
+(def rempick
+  (fn [n lat]
+    (cond
+      (one? n) (cdr lat)
+      :else (cons (car lat) (rempick (sub1 n) (cdr lat))))))
+
+(def rember*
+  (fn [a l]
+    (cond
+      (null? l) '()
+      (atom? (car l))
+        (cond
+          (eq? a (car l)) (rember* a (cdr l))
+          :else (cons (car l) (rember* a (cdr l))))
+      :else (cons (rember* a (car l)) (rember* a (cdr l))))))
+
+(def insertR*
+  (fn [new old l]
+    (cond
+      (null? l) '()
+      (atom? (car l))
+        (cond
+          (eq? old (car l)) (cons old (cons new (insertR* new old (cdr l))))
+          :else (cons (car l) (insertR* new old (cdr l))))
+      :else (cons (insertR* new old (car l)) (insertR* new old (cdr l))))))
+
+(def occur*
+  (fn [a l]
+    (cond
+      (null? l) 0
+      (atom? (car l))
+        (cond
+          (eq? a (car l)) (add1 (occur* a (cdr l)))
+          :else (occur* a (cdr l)))
+      :else (+ (occur* a (car l)) (occur* a (cdr l))))))
+
+(def subst*
+  (fn [new old l]
+    (cond
+      (null? l) '()
+      (atom? (car l))
+        (cond
+          (eq? old (car l)) (cons new (subst* new old (cdr l)))
+          :else (cons (car l) (subst* new old (cdr l))))
+      :else (cons (subst* new old (car l)) (subst* new old (cdr l))))))
+
+(def insertL*
+  (fn [new old l]
+    (cond
+      (null? l) '()
+      (atom? (car l))
+        (cond
+          (eq? old (car l)) (cons new (cons old (insertL* new old (cdr l))))
+          :else (cons (car l) (insertL* new old (cdr l))))
+      :else (cons (insertL* new old (car l)) (insertL* new old (cdr l))))))
+
+(def member*
+  (fn [a l]
+    (cond
+      (null? l) false
+      (atom? (car l))
+        (or (eq? (car l) a) (member* a (cdr l)))
+      :else (or (member* a (car l)) (member* a (cdr l))))))
+
